5916d12fba51b09e63726c0ace0efb2d0ee6cc25
Isaque Torres <49525597+IsaqueTorres@users.noreply.github.com>
2021-09-01 11:49:25 -0300

    Update inversor.c

--- inversor.c
+++ inversor.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int numero, ultimoNumero, penultimoNumero, primeiroNumero;
+
+int main(){
+
+    printf("...::: PROGRAMA INVERSOR DE NUMEROS :::...\n");
+    printf("Insira o número que deseja inverter:");
+    printf("0 a 999\n");
+    scanf("%i", &numero);
+
+    //calculador de numeros//
+
+    ultimoNumero = numero % 10;
+    penultimoNumero = numero / 10 % 10;
+    primeiroNumero = numero / 100;
+
+    printf("Aqui esta o numero invertido %i%i%i\n", ultimoNumero, penultimoNumero, primeiroNumero);
+
+
+return 0;    
+
+}
